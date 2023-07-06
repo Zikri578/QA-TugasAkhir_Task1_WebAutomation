31e2c923446f33acc5127820c1116d38afcce752
Muhammad Zikri <56788800+Zikri578@users.noreply.github.com>
2023-07-06 18:07:37 +0700

    Update removeCart_pages.cy.ts

--- cypress/e2e/pages/removeCart_pages.cy.ts
+++ cypress/e2e/pages/removeCart_pages.cy.ts
@@ -4,3 +4,16 @@ export class removeCard{
     cy.get('[data-test="remove-sauce-labs-backpack"]').click();
     }
 }
+
+/**
+ * maksud dari codingan diatas yaitu : 
+ * 
+ *  Codingan di atas adalah kelas removeCard yang berisi metode RemBtnShop().
+ *  Metode ini digunakan untuk mengklik tombol "Remove" pada suatu produk di keranjang di situs web yang sedang diuji menggunakan Cypress.
+ *  Di dalam metode RemBtnShop(), terdapat penggunaan cy.wait(1000) untuk menunggu selama 1 detik sebelum melakukan tindakan berikutnya.
+ *  Hal ini dapat digunakan untuk memastikan kesiapan elemen yang akan diklik.
+ *  Setelah itu, menggunakan cy.get('[data-test="remove-sauce-labs-backpack"]') untuk mengambil elemen dengan atribut data-test yang bernilai remove-sauce-labs-backpack. 
+ *  Atribut ini digunakan untuk mengidentifikasi tombol "Remove" pada produk tertentu di keranjang.
+ *  Selanjutnya, digunakan click() untuk melakukan tindakan mengklik elemen tersebut, sehingga produk akan dihapus dari keranjang.
+ *  Dengan menggunakan kelas removeCard dan metode RemBtnShop(), kita dapat melakukan tindakan penghapusan produk dari keranjang pada situs yang sedang diuji.
+*/
